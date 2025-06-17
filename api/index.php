e5cddc5204cc1c6f5ab90467f427de8665dcc06f
DevSeedorf <saheedabdulganiyu01@gmail.com>
2025-06-17 03:39:08 +0100

    xc

--- api/index.php
+++ api/index.php
@@ -22,11 +22,5 @@ putenv("APP_BOOTSTRAP_CACHE_PATH=$cacheDir");
 
 // Load Laravel
 require __DIR__.'/../vendor/autoload.php';
-$app = require_once __DIR__.'/../bootstrap/app.php';
+require_once __DIR__.'/../bootstrap/app.php';
 
-$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
-$response = $kernel->handle(
-    $request = Illuminate\Http\Request::capture()
-);
-$response->send();
-$kernel->terminate($request, $response);
