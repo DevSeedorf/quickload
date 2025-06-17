ffcfa463438e98a08e7f67d973b873f0586c1612
DevSeedorf <saheedabdulganiyu01@gmail.com>
2025-06-17 02:43:34 +0100

    .

--- api/index.php
+++ api/index.php
@@ -4,10 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-// Autoload dependencies
-require __DIR__ . '/../vendor/autoload.php';
-
-$app = require_once __DIR__ . '/../bootstrap/app.php';
+require __DIR__.'/../public/index.php';
 
 // Handle the request
 $kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
